58563285d432b1225e5e8032da3bfe950e13e8db
José Guilherme <joseguilhermefmoura@gmail.com>
2021-04-14 10:30:00 -0300

    :arrow_up: Atualizado forms.py

--- visitors/forms.py
+++ visitors/forms.py
@@ -6,3 +6,19 @@ class VisitorForm(forms.ModelForm):
     class Meta:
         model = Visitor
         fields = ['complete_name','cpf', 'birthday_date', 'house_num', 'vehicle_plate']
+
+        error_messages = {
+            'complete_name' : {
+                'required' : 'O nome completo do visitante é obrigatório para o registro.'
+            },
+            'cpf' : {
+                'required' : 'O CPF do visitante é obrigatório para o registro.'
+            },
+            'birthday_date' : {
+                'required' : 'A data de nascimento do visitante é obrigatória para o registro.',
+                'invalid' : 'A data de nascimento do visitante deve ser informada no formato: DD/MM/AAAA'
+            },
+            'house_num' : {
+                'required' : 'O número da casa que será visitada é obrigatório para o registro.'
+            },
+        }
