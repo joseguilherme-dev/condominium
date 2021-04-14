d6517c4d51870fb27aae4e8fd316c4f47eb69bd7
José Guilherme <joseguilhermefmoura@gmail.com>
2021-04-13 22:56:06 -0300

    :arrow_up: Update views.py

--- visitors/views.py
+++ visitors/views.py
@@ -1,3 +1,27 @@
-from django.shortcuts import render
+from django.contrib import messages
+from django.shortcuts import render, redirect
+from visitors.forms import VisitorForm
 
-# Create your views here.
+def register_visitor(request):
+    form = VisitorForm()
+
+    if request.method == "POST":
+        form = VisitorForm(request.POST)
+
+        if form.is_valid():
+            visitor = form.save(commit=False)
+
+            visitor.related_concierge = request.user.concierge
+            visitor.save()
+
+            messages.success(
+                request,
+                "O visitante foi registrado com sucesso."
+            )
+
+    context = {
+        "form": form,
+        "page_name": "Registrar Visitantes",
+    }
+
+    return render(request, "visitors/register_visitor.html", context)
