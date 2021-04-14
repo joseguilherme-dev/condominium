0d9211b77592b377594f7911f25ed136af272cd8
José Guilherme <joseguilhermefmoura@gmail.com>
2021-04-13 22:53:52 -0300

    :arrow_up: Update settings.py

--- condominium_control/settings.py
+++ condominium_control/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    # Default apps
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -39,9 +40,13 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
+    # Created apps
     'users',
     'concierges',
     'visitors',
+
+    # Installed apps
+    'widget_tweaks',
 ]
 
 MIDDLEWARE = [
@@ -125,7 +130,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_FILES_DIRS = [
+STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static")
 ]
 
