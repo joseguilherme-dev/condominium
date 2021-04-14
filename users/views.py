432001a1313466408009da3784937ad78d9baef2
José Guilherme <joseguilhermefmoura@gmail.com>
2021-04-13 22:54:13 -0300

    :arrow_up: Update views.py

--- users/views.py
+++ users/views.py
@@ -1,9 +1,13 @@
 from django.shortcuts import render
+from visitors.models import Visitor
 
 def index(request):
+    all_visitors = Visitor.objects.all()
+
     # Django context dict, 
     context = {
         "page_name": "Início",
+        "visitors": all_visitors,
     }
 
     return render(request, "index.html", context)
